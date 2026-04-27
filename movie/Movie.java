456c4d20f3d8de6ee6482376512f1563b7d5fe83
Vanshika joshi <vanshikajoshi072@gmail.com>
2026-04-27 21:41:16 +0530

    added files

---
+++ movie/Movie.java
@@ -0,0 +1,25 @@
+package movie;
+
+public class Movie {
+    private String name;
+    private int year;
+    private double rating;
+
+    public Movie(String name, int year, double rating) {
+        this.name = name;
+        this.year = year;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+}
